422e0ee50791e7f598f7a839024c288e8478d07f
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-12-11 13:05:25 +0100

    Initial commit

---
+++ QuizAssignment1/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms;
+
+namespace QuizAssignment1
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly MainViewModel viewModel = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+    }
+}
