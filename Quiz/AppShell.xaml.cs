3960f976adf2fce7233a6b32802e046ad508abd8
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-06-17 10:51:47 +0200

    MAUI Update

---
+++ Quiz/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Quiz;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Quiz/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Quiz;
+
+public partial class MainPage : ContentPage
+{
+    private readonly MainViewModel viewModel = new MainViewModel();
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = viewModel;
+    }
+}
